fa37332195f019fe7005dcc60f42cb06acd63a23
Maharshi <42892220+Maharshi-B@users.noreply.github.com>
2019-11-01 00:06:35 +0530

    first commit

---
+++ weconnect/groups/models.py
@@ -0,0 +1,46 @@
+from django import template
+from django.db import models
+from django.utils.text import slugify
+import mistune
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+User = get_user_model()
+
+register = template.Library()
+# Create your models here.
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False,
+                                        default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = mistune.markdown(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single', kwargs={'slug': self.slug})
+
+    class Meta():
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(
+        Group, related_name='memberships', on_delete="Cascade")
+    user = models.ForeignKey(
+        User, related_name='user_groups', on_delete="Cascade")
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta():
+        unique_together = ('group', 'user')
