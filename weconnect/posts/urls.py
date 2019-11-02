76099d64d912295074260abb2030f088828dc538
Maharshi <42892220+Maharshi-B@users.noreply.github.com>
2019-11-02 16:27:22 +0530

    fixed post detail

--- weconnect/posts/urls.py
+++ weconnect/posts/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="all"),
     path('new/', views.CreatePost.as_view(), name="create"),
     path('by/(?P<username>[-\w]+)/', views.UserPosts.as_view(),
          name="for_user"),
